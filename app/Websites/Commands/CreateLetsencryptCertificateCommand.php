9d9353986481cd89a71ab1ebeece72aa8bbe7ef0
Rasmus Sørensen - iMac <xxr@live.dk>
2020-05-27 08:58:31 +0200

    fix errors in command

--- app/Websites/Commands/CreateLetsencryptCertificateCommand.php
+++ app/Websites/Commands/CreateLetsencryptCertificateCommand.php
@@ -4,6 +4,7 @@ namespace App\Websites\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class CreateLetsencryptCertificateCommand extends Command
 {
@@ -38,29 +39,29 @@ class CreateLetsencryptCertificateCommand extends Command
      */
     public function handle()
     {
-        $domain = $this->argument('domain');
-
-        if (empty($domain)) {
-            throw new \RuntimeException("The argument 'domain' is required.");
-        }
-
         $email = $this->option('email');
 
         if (empty($email)) {
             throw new \RuntimeException("The option '--email' is required.");
         }
 
+        $domain = $this->argument('domain');
         $safeDomain = escapeshellarg($domain);
         $safeEmail = escapeshellarg($email);
         $bin = escapeshellarg(base_path('bin/certbot_manager'));
         $configFile = escapeshellarg(base_path('digitalocean.ini'));
 
         // First delete any current certifcate
-        $this->call('letsencrypt:delete', ['user' => $domain]);
+        $this->call('letsencrypt:delete', ['domain' => $domain]);
+
+        $cmd = sprintf('bash %s certonly --dns-digitalocean --dns-digitalocean-credentials %s -m %s -d %s -d www.%s 2>&1', $bin, $configFile, $safeEmail, $safeDomain, $safeDomain);
+        if (Str::contains($domain, '*.')) {
+            $cmd = sprintf('bash %s certonly --dns-digitalocean --dns-digitalocean-credentials %s -m %s -d %s 2>&1', $bin, $configFile, $safeEmail, $safeDomain);
+        }
 
         // Then create new certifcate
         $lastLine = exec(
-            sprintf('bash %s certonly --dns-digitalocean --dns-digitalocean-credentials %s -m %s -d %s -d www.%s 2>&1', $bin, $configFile, $safeEmail, $safeDomain, $safeDomain),
+            $cmd,
             $retArr,
             $retVal
         );
